5ba307b9f75c27b68ff9aea9d9030186d2048e60
bessx <1159066+bessx@users.noreply.github.com>
2022-07-26 21:31:22 -0700

    fix logger spaces

--- src/chopper/Chopper.py
+++ src/chopper/Chopper.py
@@ -83,12 +83,10 @@ def chopall(mols):
         #
         results = brick_db.addAll([Brick.Brick(b, mol, suffix = index) for index, b in enumerate(bricks)])
     
-        log.debug(f'Added {len(results)} TC-unique bricks; \
-                             {len(bricks) - len(results)} were TC-redundant')
+        log.debug(f'Added {len(results)} TC-unique bricks; \t{len(bricks) - len(results)} were TC-redundant')
 
         results = linker_db.addAll([Linker.Linker(ell, mol, suffix = index) for index, ell in enumerate(linkers)])
 
-        log.debug(f'Added {len(results)} TC-unique linkers; \
-                             {len(linkers) - len(results)} were TC-redundant')
+        log.debug(f'Added {len(results)} TC-unique linkers; \t{len(linkers) - len(results)} were TC-redundant')
 
     return brick_db, linker_db
