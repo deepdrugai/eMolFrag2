a251e2fc3863726340e160b24a47bdcc3b9c33ea
bessx <1159066+bessx@users.noreply.github.com>
2022-07-27 22:47:28 -0700

    change pathing and remove utilities imports

--- unittests/utilities.py
+++ unittests/utilities.py
@@ -4,9 +4,9 @@
 
 from rdkit import Chem
 from pathlib import Path
-import argparse
+# import argparse
 # from eMolFrag2.src.utilities import constants
-from eMolFrag2.src.input import Options
+# from eMolFrag2.src.input import Options
 
 
 def fileToString(file):
@@ -30,7 +30,7 @@ def convertToRDkit(contents, extension):
 
     if (extension == ".mol2"):
       try: 
-        # default parameters: (str)contents 
+        # default parameters: (str)contents
         #                     sanitize=True
         #                     removeHs=True
         #                     cleanupSubstructures=True
@@ -65,44 +65,44 @@ def getRDKitMolecule(path, extension=None):
     content = fileToString(path)
     return convertToRDkit(content, extension)
     
-def createParser():
-  parser = argparse.ArgumentParser(description = 'eMolFrag2')
-  #
-  # eMolFrag arguments
-  #
-  parser.add_argument('-' + Options.INPUT_ARG,
-                      type = str,
-                      help = 'Input path to molecules to fragment')
-  parser.add_argument('-' + Options.OUTPUT_ARG,
-                      type = str,
-                      help = 'Output path for fragments (existing files will be overwritten.)')
+# def createParser():
+#   parser = argparse.ArgumentParser(description = 'eMolFrag2')
+#   #
+#   # eMolFrag arguments
+#   #
+#   parser.add_argument('-' + Options.INPUT_ARG,
+#                       type = str,
+#                       help = 'Input path to molecules to fragment')
+#   parser.add_argument('-' + Options.OUTPUT_ARG,
+#                       type = str,
+#                       help = 'Output path for fragments (existing files will be overwritten.)')
 
-  parser.add_argument('-' + Options.CONFIGURATION_FILE_ARG,
-                      type = str,
-                      help = 'Configuration file: .emf extension required.)')
+#   parser.add_argument('-' + Options.CONFIGURATION_FILE_ARG,
+#                       type = str,
+#                       help = 'Configuration file: .emf extension required.)')
 
-  parser.add_argument('-' + Options.ALL_FRAGMENTS_ARG,
-                      action = 'store_true',
-                      default = False,
-                      help = 'Output all fragments (all non-unique molecules)')
+#   parser.add_argument('-' + Options.ALL_FRAGMENTS_ARG,
+#                       action = 'store_true',
+#                       default = False,
+#                       help = 'Output all fragments (all non-unique molecules)')
 
-  parser.add_argument('-' + Options.INDIVIDUAL_FILE_ARG,
-                      action = 'store_true',
-                      default = False,
-                      help = 'Fragment will be output in their own individual files')
+#   parser.add_argument('-' + Options.INDIVIDUAL_FILE_ARG,
+#                       action = 'store_true',
+#                       default = False,
+#                       help = 'Fragment will be output in their own individual files')
   
-  return parser
+#   return parser
   
 #
 #
 # Printing to console ; can modify later to dump stream to other locations
 #
 # 
-def emit(level, s):
-    print("  " * level + s)
+# def emit(level, s):
+#     print("  " * level + s)
     
-def emitError(level, s):
-    print("  " * level + "Error:", s)
+# def emitError(level, s):
+#     print("  " * level + "Error:", s)
 
-def emitWarning(level, s):
-    print("  " * level + "Warning:", s)
+# def emitWarning(level, s):
+#     print("  " * level + "Warning:", s)
