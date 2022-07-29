2f477e6b94e371ae911d2f62055ef41011986bcd
bessx <1159066+bessx@users.noreply.github.com>
2022-07-28 23:08:04 -0700

    rewrite test_deconstructor

--- unittests/test_deconstructor.py
+++ unittests/test_deconstructor.py
@@ -1,46 +1,35 @@
-from pathlib import Path
 import pytest
+from pathlib import Path
 from rdkit import Chem
-from eMolFrag2.src.utilities.logging import log
 
-#import os
 from eMolFrag2.src.input import MoleculeReader
 from eMolFrag2.src.chopper import Deconstructor
+from eMolFrag2.src.utilities.logging import log
 
-rel_path = "mol2-test"
 
-prob_files = ['DB01328.mol2', 'DB00779.mol2',
-              'DB01155.mol2', 'DB00845.mol2', 'DB00430.mol2', 'DB00467.mol2', 'DB01405.mol2',
-              'DB01051.mol2', 'DB00537.mol2', 'DB01137.mol2', 'DB00817.mol2',
-              'DB00229.mol2', 'DB00487.mol2', 'DB00267.mol2',
-              'DB01329.mol2', 'DB00923.mol2', 'DB00218.mol2',
-              'DB01165.mol2', 'DB01059.mol2', 'DB01327.mol2',
-              'DB00274.mol2', 'DB01044.mol2', 'DB01208.mol2',
-              'DB00978.mol2', 'DB01326.mol2', ]
-
+cwd = Path(__file__).parents[1] / "test/mol2-test"
 
 @pytest.fixture
-def kekulize_files():
-    k_files = ['DB00827.mol2']
-    return k_files
+def mol2_files():
+    files = []
+    for file in cwd.iterdir():
+        files.append(file)
+    return files
 
 
-def test_deconstruct(kekulize_files):
+def test_deconstruct(mol2_files):
 
-    for mol_path in kekulize_files:
+    for file in mol2_files:
+        # file_path = cwd / mol_path
 
-        cwd = Path(__file__).parents[1] / "test" / rel_path
-        file_path = cwd / mol_path
-
-        log.debug(f"file path: {file_path}")
+        log.info(f"File: {file}")
 
         # create rdkit molecule from molPath
-        rdkit_mol = MoleculeReader.getRDKitMolecule(
-            file_path, Path(file_path).suffix)
+        rdkit_mol = MoleculeReader.getRDKitMolecule(file)
 
         if rdkit_mol is None:
-            log.error(f"RDKit Molecule Object is None")
-            assert False
+            # assert False
+            continue
 
         # remove hydrogen
         rdkit_mol = Chem.RemoveAllHs(rdkit_mol, sanitize=True)
@@ -48,49 +37,31 @@ def test_deconstruct(kekulize_files):
         # get fragments and sets of chopping points
         bricks, linkers, snips = Deconstructor.deconstruct(rdkit_mol)
 
-        fragments = []
-
         # count number of atoms in bricks set
-        b_count = 0
-        for b in bricks:
-            b_count += len(b)
-            for t in b:
-                fragments.append(t)
+        b_count = sum(len(b) for b in bricks)
+        fragments = [t for b in bricks for t in b]
 
         # count number of atoms in linkers set
-        l_count = 0
-        for l in linkers:
-            l_count += len(l)
-            for t in l:
-                fragments.append(t)
+        l_count = sum(len(l) for l in linkers)
+        fragments += [t for l in linkers for t in l]
 
-        log.info(f'# of atoms in the molecule: { len(rdkit_mol.GetAtoms()) }')
-        log.info(f'# of atoms after deconstruct:{ b_count + l_count }')
+        log.debug(f'# of atoms in the molecule: { len(rdkit_mol.GetAtoms()) }')
+        log.debug(f'# of atoms after deconstruct:{ b_count + l_count }')
 
         # Check if there is any overlapped atoms - atoms
-        atoms = []
-        for s in snips:
-            atoms += s
-
+        atoms = [s for t in snips for s in t]
         uniqueAtoms = set(atoms)
+        diff = sum(1 for atom in uniqueAtoms if not atom in fragments)
 
-        diff = 0
-        for atom in uniqueAtoms:
-            if not atom in fragments:
-                diff += 1
-
-        log.info(f'Atoms not in bricks or linkers: {diff}')
-
-        # difference = len(atoms) - len(uniqueAtoms)
-        log.info(f'Atoms in Final Snips: {atoms}')
-        log.info(f'Unique atoms in final snips: {uniqueAtoms}')
+        log.debug(f'Atoms not in bricks or linkers: {diff}')
+        log.debug(f'Atoms in Final Snips: {atoms}')
+        log.debug(f'Unique atoms in final snips: {uniqueAtoms}')
 
         #
         # Test Normal Case: atoms do not overlap
         #   fragments contain the same number of atoms as the original molecule
         #
         if diff == 0:
-            # print("Running Normal Tests")
             assert b_count + l_count == len(rdkit_mol.GetAtoms())
 
         #
@@ -99,6 +70,5 @@ def test_deconstruct(kekulize_files):
         #
         else:
             # total number of atoms in bricks and linkers
-            #      should equal to number of atoms in original molecule - number of overlapped atoms
-            # print("Running Tests with Overlapped Atoms")
+            # should equal to number of atoms in original molecule - number of overlapped atoms
             assert b_count + l_count == len(rdkit_mol.GetAtoms()) - diff
