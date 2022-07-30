955026caaf683be8e2ce73f23e21a0f86e19c630
haydengemeinhardt <haydengemeinhardt@gmail.com>
2022-07-30 01:39:37 -0500

    added coverage, clean up tests

--- unittests/test_molecule_file_reader.py
+++ unittests/test_molecule_file_reader.py
@@ -10,12 +10,13 @@ from eMolFrag2.src.input import Options, MoleculeFileReader
     (["sdf"], [0]),
     (["pbd"], [5]),
     (["mol"], [5]),
+    (["path_not_exists"], [0]),#test if directory doesn't exist
+    (["mol/DB00415.mol"], [0]) #test if not a directory
 ])
 
 def test_get_files(input, expected):
     cwd = Path(__file__).parent / "data/db-files"
     for m, e in zip(input, expected):
-        filePath = Path(__file__).parent.joinpath("config.emf")
         sys.argv = ['eMolFrag2/src/eMolFrag.py', '-i', str(cwd/m), '-o', '/content/out']
         options = Options.Options()
         assert len(MoleculeFileReader.getFiles(options)) == e
