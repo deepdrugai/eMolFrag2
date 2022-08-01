4c3c572005451fd74f0ea722eb2721f7d5da2fca
Magesh Rajasekaran <rmagesh148@gmail.com>
2022-08-01 18:00:01 -0500

    refactored and added fragment

--- unittests/test_molecule.py
+++ unittests/test_molecule.py
@@ -1,32 +1,46 @@
+import py
 import pytest
 from pathlib import Path
-from eMolFrag2.src.representation.Molecule import Molecule
 from eMolFrag2.src.input.MoleculeReader import to_mol
 from eMolFrag2.src.utilities.logging import log
 
 dir = Path(__file__).parent / "data"
+ms = ["similarPairSMI/3/DB12447.smi", "uniqueMol(SMI)/DB00415.smi"]
 
-@pytest.mark.parametrize("mol_path", [("similarPairSMI/3/DB12447.smi"), ("uniqueMol(SMI)/DB00415.smi")])
-def test_mol_file_parent_name(mol_path):
+@pytest.fixture()
+def file_names():
+    file_name = []
+    for m in ms:
+        name = m.split('/')
+        file_name.append(name[len(name)-1])
+    return file_name
+
+
+@pytest.fixture()
+def mols():
+    list_mols = []
+    for m in ms:
+        path = dir / m
+        list_mols.append(to_mol(path))
+    return list_mols
+
+
+def test_mol_file_parent_name(mols, file_names):
     """Testing the molecule object"""
-    log.info(f"Molecule Path : {dir / mol_path}")
-    path = dir / mol_path
-    mol = to_mol(path)
-    log.info(
-        f"Parent name : {mol.getParent()}; File Name: {mol.getFileName()}")
-    assert mol.getParent() is None and mol.getFileName() is not None
+    for m in mols:
+        log.info(
+            f"Parent name : {m.getParent()}; File Name: {m.getFileName()}")
+        assert m.getParent() is None and m.getFileName() in file_names
 
 
-@pytest.mark.parametrize("mol_path", [("similarPairSMI/3/DB12447.smi"), ("uniqueMol(SMI)/DB00415.smi")])
-def test_clear_prop_to_sdf(mol_path):
+def test_clear_prop_to_sdf(mols):
     """Testing the clear properties"""
-    path = dir / mol_path
-    mol = to_mol(path)
-    log.info(f"RDKit Object: {mol.getRDKitObject().GetPropNames()}")
-    mol_to_sdf = mol._toSDF()
-    log.info(f"RDKit Object: {type(mol_to_sdf)}")
-    mol_clear = mol.clearProperties()
-    assert mol_clear is None and type(mol_to_sdf) is str
+    for m in mols:
+        log.info(f"RDKit Object: {m.getRDKitObject().GetPropNames()}")
+        mol_to_sdf = m._toSDF()
+        log.info(f"RDKit Object: {type(mol_to_sdf)}")
+        mol_clear = m.clearProperties()
+        assert mol_clear is None and type(mol_to_sdf) is str
 
 
 @pytest.mark.parametrize("mol_path1, mol_path2, expected", [("uniqueMol(SMI)/DB00415.smi", "uniqueMol(SMI)/DB01208.smi", False),
@@ -36,6 +50,13 @@ def test_eq_mols(mol_path1, mol_path2, expected):
     path2 = dir / mol_path2
     mol1 = to_mol(path1)
     mol2 = to_mol(path2)
-    log.debug(f"Molecule as String: {str(mol1) == str(mol2)}")
-    log.debug(f"String Molecule: {str(mol1)} and {str(mol2)}")
+    log.info(f"Molecule as String: {str(mol1) == str(mol2)}")
     assert (mol1 == mol2) == expected and (str(mol1) == str(mol2)) == expected
+
+
+def test_fragment_name(mols):
+    """Test the Fragment Name"""
+    for m in mols:
+        frag_name = m.makeFragmentFileName(file_name=m.getFileName())
+        log.info(f"Fragment Name: {frag_name}")
+        assert frag_name.split('-')[1] == m.getFileName()
