a50380d0417dc6cf5e2f1c944211be1bbc0684cb
bessx <1159066+bessx@users.noreply.github.com>
2022-08-01 18:48:35 -0700

    mols is None ~> if not mols

--- unittests/test_molecule_reader.py
+++ unittests/test_molecule_reader.py
@@ -4,30 +4,26 @@ from eMolFrag2.src.input.MoleculeReader import getMolecules, to_mol
 from eMolFrag2.src.utilities.logging import log
 
 cwd = Path(__file__).parent / "data/db-files"
-failed_mol_path = Path(__file__).parent.parent /"test/mol2-test"
-
-#Read files in from unittests/data/db-files for mol2, smi, sdf, pbd, mol (5 tests)
-#For each test, if one of those objects returns a None rdkitObject, test fails
-
+failed_mol_path = Path(__file__).parent.parent / "test/mol2-test"
 @pytest.mark.parametrize("input", [
     (["mol2"]),
     (["smi"]),
     (pytest.param(["sdf"],marks=pytest.mark.xfail(raises=NotImplementedError))),
     (["pbd"]),
     (["mol"]),
-    (["failed"])
-])
-def test_to_mol(input): #single file to mol
+    (["failed"]),
+])  # fmt: skip
+def test_to_mol(input):  # single file to mol
     for suffix in input:
         file_path = cwd.joinpath(suffix)
-        if suffix == "failed": #testing for a failed mol that has a supported format
-            failed_mol = to_mol(failed_mol_path /"DB01059.mol2")
+        if suffix == "failed":  # testing for a failed mol that has a supported format
+            failed_mol = to_mol(failed_mol_path / "DB01059.mol2")
             assert failed_mol.rdkitObject == None
-        else: #checking if all mols in supported format test folders return a conversion
+        else:  # checking if all mols in supported format test folders return a conversion
             for current_file in file_path.iterdir():
                 mol = to_mol(file_path / current_file)
-                
-                #tesing for an unsupported format
+
+                # tesing for an unsupported format
                 assert mol.rdkitObject == None if suffix == ("sdf") else mol.rdkitObject != None
 
 
@@ -37,10 +33,10 @@ def test_to_mol(input): #single file to mol
     (["sdf"], [0]),
     (["pbd"], [4]),
     (["mol"], [5]),
-    (["failed"], [0])
-])
-def test_get_files(input, expected): #multiple files to mol
-    failed_mols = ['DB01059.mol2', 'DB01326.mol2', 'DB00229.mol2', 'DB00779.mol2', 'DB00355.mol2', 'DB00430.mol2'] 
+    (["failed"], [0]),
+])  # fmt: skip
+def test_get_files(input, expected):  # multiple files to mol
+    failed_mols = ["DB01059.mol2", "DB01326.mol2", "DB00229.mol2", "DB00779.mol2", "DB00355.mol2", "DB00430.mol2"]
     for suffix, e in zip(input, expected):
         files = []
         if suffix == "failed":
@@ -54,12 +50,13 @@ def test_get_files(input, expected): #multiple files to mol
                 files.append(file_path / current_file.name)
             mols = getMolecules(files)
             log.debug(mols)
-            
+
             # Assert that length of mols == e
             assert len(mols) == e
-            
-            if not suffix == "sdf": #unsupported format, so should return an empty list
-                #If any of the returned mols have rdkitObject==None, then the file was not read properly
+
+            if not suffix == "sdf":  # unsupported format, so should return an empty list
+                # If any of the returned mols have rdkitObject==None, then the file was not read properly
                 assert all(mol.rdkitObject != None for mol in mols)
 
-#test nonexistent file, test direct instead of file, test failed mol2s?
+
+# test nonexistent file, test direct instead of file, test failed mol2s?
