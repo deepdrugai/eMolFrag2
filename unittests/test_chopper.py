26e35156f5e2d54a0e799df433336bc3ec96a1fa
bessx <1159066+bessx@users.noreply.github.com>
2022-08-04 18:14:32 -0700

    test_chopper fixes part 1

--- unittests/test_chopper.py
+++ unittests/test_chopper.py
@@ -1,45 +1,41 @@
-# from cmath import exp
-# from isort import file
 import pytest
 from pathlib import Path
-# from rdkit import Chem
-# from eMolFrag2.src.utilities import constants #, tc
-# from eMolFrag2.src.utilities.logging import log
+
+from eMolFrag2.src.utilities.logging import log
 from eMolFrag2.src.chopper import Chopper
-# from eMolFrag2.src.representation import MoleculeDatabase as MDB
-# from eMolFrag2.src.representation import Brick
-# from eMolFrag2.src.representation import Linker 
 from eMolFrag2.src.input.MoleculeReader import to_mol
 
-cwd = Path(__file__).parent / "data"
+cwd = Path(__file__).parent / "data/9070-non-c2-c2-double-bond"
+
 
 @pytest.fixture
 def output():
-  expectedLinks = [
-    "l-CHEMBL9070.mol2-000.sdf"
-    ]    
-    
-  expectedBricks = [
-    "b-CHEMBL9070.mol2-000.sdf", 
-    "b-CHEMBL9070.mol2-001.sdf", 
-    "b-CHEMBL9070.mol2-002.sdf", 
-    "b-CHEMBL9070.mol2-003.sdf", 
-    "b-CHEMBL9070.mol2-004.sdf"
-    ]
-  return expectedBricks, expectedLinks
+    expectedLinks = ["l-CHEMBL9070.mol2-000.sdf"]
 
-@pytest.mark.parametrize("input", [
-  # Molecule
-  ([to_mol(cwd / "db-files/mol2-b-l/input/CHEMBL9070.mol2")])
-])
+    expectedBricks = [
+        "b-CHEMBL9070.mol2-000.sdf",
+        "b-CHEMBL9070.mol2-001.sdf",
+        "b-CHEMBL9070.mol2-002.sdf",
+        "b-CHEMBL9070.mol2-003.sdf",
+        "b-CHEMBL9070.mol2-004.sdf",
+    ]
+    return expectedBricks, expectedLinks
+
+
+@pytest.mark.parametrize("input", [[to_mol(cwd / "CHEMBL9070.mol2")]])
 def test_chopall(input, output):
-# get bricks and linkers from input 
+    # get bricks and linkers from input
     bricks, links = Chopper.chopall(input)
     eBrick, eLink = output
-# compare if expected links are equal to actual links
+    log.info(f"{[x for x in bricks.GetAllMolecules()] = }")
+    log.info(f"{links.GetAllMolecules() = }")
+    log.info(f"{eBrick = }, {eLink = }")
+    # compare if expected links are equal to actual links
     for line in links.GetAllMolecules():
-      assert eLink.__contains__(line.toSDF()[0:25])
- 
-# compare if expected bricks are equal to actual bricks
+        log.info(f"links {line = }")
+        assert eLink.__contains__(line.toSDF()[0:25])
+
+    # compare if expected bricks are equal to actual bricks
     for line in bricks.GetAllMolecules():
-      assert eBrick.__contains__(line.toSDF()[0:25]) 
+        log.info(f"bricks {line = }")
+        assert eBrick.__contains__(line.toSDF()[0:25])
