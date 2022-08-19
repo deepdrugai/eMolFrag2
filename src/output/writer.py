67be415f5c1909f2f3291702f09d4a0944a647fc
bessx <1159066+bessx@users.noreply.github.com>
2022-08-18 23:58:10 -0500

    writer cleanup and close issue 21.

--- src/output/writer.py
+++ src/output/writer.py
@@ -5,86 +5,90 @@ from pathlib import Path
 from eMolFrag2.src.utilities.logging import log
 from eMolFrag2.src.utilities import constants
 
+
 def prepareDirectory(out_path):
     """
-        If the directory does not exist, create it.
-        If the directory does exist, clean it.        
+    If the directory does not exist, create it.
+    If the directory does exist, clean it.
     """
     if out_path.is_file():
-        log.error(f'Output path {str(out_path)} is a file, not a directory.')
-        raise ValueError(f'Malformed output specification path {str(out_path)}')
+        log.error(f"Output path {str(out_path)} is a file, not a directory.")
+        raise ValueError(f"Malformed output specification path {str(out_path)}")
 
-    # Rename the directory, if needed    
+    # Rename the directory, if needed
     new_path = out_path
-    i=1
+    i = 1
     while new_path.exists():
-        log.warning(f'Output path {str(new_path)} exists; {str(out_path)}-{i} will be used.')
+        log.warning(f"Output path {str(new_path)} exists; {str(out_path)}-{i} will be used.")
         new_path = out_path.parent / f"{out_path.name}-{i}"
-        i+=1
+        i += 1
     else:
-        log.info(f'Output path {str(new_path)} does not exist; will be created.')
+        log.info(f"Output path {str(new_path)} does not exist; will be created.")
 
     out_path = new_path
     del new_path, i
-    
+
     # Create the diretory
     out_path.mkdir()
 
     # Return new path
     return out_path
 
-def writeSingleFile(indicator, name, out_dir, mols, extension = constants.SDF_FORMAT_EXT):
-    """
-        indicator --  'u' --> unique or 'a' --> all
-        name -- main part of the output file: 'bricks' or 'linkers'
-        out_dir -- output directory path
-        mols -- the actual Molecule objects to write
-    """
-    file_name = f'{indicator}{name}{extension}'
 
-    log.debug(f'Writing file {out_dir}/{file_name}')
+def writeSingleFile(indicator, name, out_dir, mols, extension=constants.SDF_FORMAT_EXT):
+    """
+    indicator --  'u' --> unique or 'a' --> all
+    name -- main part of the output file: 'bricks' or 'linkers'
+    out_dir -- output directory path
+    mols -- the actual Molecule objects to write
+    """
+    file_name = f"{indicator}{name}{extension}"
+
+    log.debug(f"Writing file {out_dir}/{file_name}")
 
     # Delimiter needed? Or does SDWriter put it there?
-    text = '\n'.join([mol.toSDF() for mol in mols])
+    text = "\n".join([mol.toSDF() for mol in mols])
 
     out_path = out_dir / file_name
 
-    with out_path.open('w') as f:
-            f.write(text)
+    with out_path.open("w") as f:
+        f.write(text)
 
-def writeIndividualFiles(out_dir, mols, extension = constants.SDF_FORMAT_EXT):
+
+def writeIndividualFiles(out_dir, mols, extension=constants.SDF_FORMAT_EXT):
     """
-        indicator --  'u' --> unique or 'a' --> all
-        name -- main part of the output file: 'bricks' or 'linkers'
-        out_dir -- output directory path
-        mols -- the actual Molecule objects to write
+    indicator --  'u' --> unique or 'a' --> all
+    name -- main part of the output file: 'bricks' or 'linkers'
+    out_dir -- output directory path
+    mols -- the actual Molecule objects to write
     """
-    for mol in mols:    
+    for mol in mols:
         out_path = out_dir / mol.getFileName()
-        with out_path.open('w') as f:
+        with out_path.open("w") as f:
             f.write(mol.toSDF())
+
 
 def write(options, brick_db, linker_db):
     """
-        Main output routine
-        The focus is what fragments (unique OR all) and format how to
-        output it (many files OR a single file).
+    Main output routine
+    The focus is what fragments (unique OR all) and format how to
+    output it (many files OR a single file).
     """
     out_dir = Path(options.OUTPUT_PATH)
     out_dir = prepareDirectory(out_dir)
 
-    log.debug(f'Writing to directory {str(out_dir)}')
+    log.debug(f"Writing to directory {str(out_dir)}")
 
     bricks_to_write = []
     linkers_to_write = []
-    indicator = ''
+    indicator = ""
 
     # All fragments wanted
     if options.ALL_FRAGMENTS:
         indicator = constants.FILE_OUTPUT_ALL_INDICATOR
         bricks_to_write = brick_db.GetAllMolecules()
         linkers_to_write = linker_db.GetAllMolecules()
-        
+
     # Only unique fragments wanted
     else:
         indicator = constants.FILE_OUTPUT_UNIQUE_INDICATOR
