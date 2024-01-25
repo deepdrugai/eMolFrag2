9988e083bcb295ce91fb46b671a3d189e3d2d86f
bessx <1159066+bessx@users.noreply.github.com>
2024-01-25 00:27:28 -0600

    Accept individual files as input

--- src/input/MoleculeFileReader.py
+++ src/input/MoleculeFileReader.py
@@ -4,29 +4,43 @@ from pathlib import Path
 from eMolFrag2.src.utilities import constants
 from eMolFrag2.src.utilities.logging import log
 
+
 def getFiles(options):
     """
-        From the input directory specified, acquire all applicable molecule files
+    From the input directory specified, acquire all applicable molecule files
     """
     folderPath = Path(options.INPUT_PATH)
-      
+    files = []
+    bad_files = []
+
     # Non-existing directory means no files to process
     if not folderPath.exists():
-        log.error(f'Input path {options.INPUT_PATH} does not exist.')
+        log.error(f"Input path {options.INPUT_PATH} does not exist.")
         return []
 
     # Path is not a directory
     if not folderPath.is_dir():
-        log.error(f'Input path {options.INPUT_PATH} is not a directory. Did you mean: {folderPath.parent} ?')
-        return []
-     
-    files = []
-    bad_files = []
+        log.error(f"Input path {options.INPUT_PATH} is not a directory. Did you mean: {folderPath.parent} ?")
+        current_file = folderPath
+        folderPath = folderPath.parent
+        # if the file extension is not a supportedd format, add the file to the bad file list, otherwise add it to the file list
+        extension = current_file.suffix
+        if extension in constants.ACCEPTED_FORMATS:
+            files.append(folderPath / current_file.name)
+        else:
+            bad_files.append(current_file)
+
+        # Report unacceptable files
+        if bad_files:
+            log.warning(f'emolFrag2 only accepts the following formats {", ".join(constants.ACCEPTED_FORMATS)}')
+            log.warning(f'The following files will be ignored: {", ".join([bf.name for bf in bad_files])}')
+
+        return files
+        # return []
 
     # grab each file with acceptable molecule extension
     for current_file in folderPath.iterdir():
-      
-        #if the file extension is not a supportedd format, add the file to the bad file list, otherwise add it to the file list
+        # if the file extension is not a supportedd format, add the file to the bad file list, otherwise add it to the file list
         extension = current_file.suffix
         if extension in constants.ACCEPTED_FORMATS:
             files.append(folderPath / current_file.name)
@@ -37,8 +51,9 @@ def getFiles(options):
     if bad_files:
         log.warning(f'emolFrag2 only accepts the following formats {", ".join(constants.ACCEPTED_FORMATS)}')
         log.warning(f'The following files will be ignored: {", ".join([bf.name for bf in bad_files])}')
-      
+
     return files
+
 
 #
 # Given a configuration file, return the file path
@@ -46,11 +61,10 @@ def getFiles(options):
 def acquireConfigurationFile(usr_file):  # pragma: no cover
     filePath = Path(usr_file)
 
-    #if the folder path doesnt exist, exit processing
+    # if the folder path doesnt exist, exit processing
     if not filePath.exists():
-
-        #print(f'Input path {usr_file} does not exist.')
+        # print(f'Input path {usr_file} does not exist.')
         log.error(f"Input path {usr_file} does not exist.")
         return None
-    
+
     return filePath
