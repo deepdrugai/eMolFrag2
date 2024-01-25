34d23e3643a9076fcd20895162c8a064d3beb188
bessx <1159066+bessx@users.noreply.github.com>
2024-01-25 02:29:43 -0600

    highlight_cleave2 ~> MolToFile

--- src/output/draw.py
+++ src/output/draw.py
@@ -1,54 +1,73 @@
+# %%
 from rdkit import Chem
-from rdkit.Chem import BRICS, Draw
-from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem import BRICS
+from rdkit.Chem.Draw import rdMolDraw2D, MolToFile, MolsToGridImage
 
-from eMolFrag2.src.chopper.Deconstructor import deconstruct
+from eMolFrag2.src.chopper.Chopper import chop
 from eMolFrag2.src.utilities.logging import log
+from IPython.display import SVG
+
 
 def draw_mol(mol, out_dir):
     # mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
     mol.RemoveAllConformers()
-    Chem.Draw.MolToFile(mol, out_dir)
+    MolToFile(mol, out_dir)
 
 
 def draw_grid_img(mol, out_dir):
-    #mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
+    # mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
     mol.RemoveAllConformers()
     frags = BRICS.BRICSDecompose(mol, returnMols=True)
-    img = Draw.MolsToGridImage(frags, molsPerRow=3, subImgSize=(250, 250), useSVG=True)
+    img = MolsToGridImage(frags, molsPerRow=3, subImgSize=(250, 250), useSVG=True)
     log.debug(type(img))
     with open(out_dir, "wb+") as outfile:
         outfile.write(img.encode("utf-8"))
 
 
 def draw_grid_img2(mol, out_dir):
-    #mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
+    # mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
     mol.RemoveAllConformers()
     single_mol_frags = BRICS.BreakBRICSBonds(mol)
     frags = Chem.GetMolFrags(single_mol_frags, asMols=True)
-    img = Draw.MolsToGridImage(frags, molsPerRow=3, subImgSize=(250, 250), useSVG=True)
+    img = MolsToGridImage(frags, molsPerRow=3, subImgSize=(250, 250), useSVG=True)
     with open(out_dir, "wb+") as outfile:
         outfile.write(img.encode("utf-8"))
 
 
-def highlight_cleave_sights(mol, out_dir):
-    #mol = Chem.MolFromSmiles('[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O')
-    mol.RemoveAllConformers()
-    bricks, linkers, snips = deconstruct(mol)
-    cleavesites = [x for xs in snips for x in xs]
-    bond_indices = []
-    for snip in snips:
-        atom1, atom2 = snip
-        bond = mol.GetBondBetweenAtoms(atom1, atom2)
-        bond_indice = bond.GetIdx()
-        bond_indices.append(bond_indice)
+def highlight_cleave_sights(mol):
+    # mol.RemoveAllConformers()
+    _, snips = chop(mol)
+    snip_atoms = [x for xs in snips for x in xs]
+    log.debug(snip_atoms)
+    snip_bonds = []
+    for atom1, atom2 in snips:
+        snip_bonds.append(mol.GetBondBetweenAtoms(atom1, atom2).GetIdx())
+    log.debug(snip_bonds)
+
     cp = Chem.Mol(mol)
     d2d = rdMolDraw2D.MolDraw2DSVG(600, 400)
     d2d.drawOptions().addAtomIndices = True
-    d2d.drawOptions().addBondIndices = False
-    d2d.DrawMolecule(cp, highlightAtoms=cleavesites, highlightBonds=bond_indices)
-    log.debug(cleavesites)
+    # d2d.drawOptions().addBondIndices = True
+    d2d.DrawMolecule(cp, highlightAtoms=snip_atoms, highlightBonds=snip_bonds)
     d2d.FinishDrawing()
-    img = d2d.GetDrawingText()   
-    with open(out_dir, "wb+") as outfile:
-        outfile.write(img.encode("utf-8"))
+    return SVG(d2d.GetDrawingText())
+    # img = d2d.GetDrawingText()
+    # with open(out_dir, "wb+") as outfile:
+    #     outfile.write(img.encode("utf-8"))
+
+
+def highlight_cleave_sights2(mol):
+    # mol.RemoveAllConformers()
+    _, snips = chop(mol)
+    snip_atoms = [x for xs in snips for x in xs]
+    log.debug(snip_atoms)
+    # snip_bonds = []
+    # for atom1, atom2 in snips:
+    #     snip_bonds.append(mol.GetBondBetweenAtoms(atom1, atom2).GetIdx())
+    snip_bonds = [mol.GetBondBetweenAtoms(atom1, atom2).GetIdx() for atom1, atom2 in snips]
+
+    MolToFile(mol, filename="/home/bess/dev/eMolFrag2/outputtest/magic5.svg", highlightBonds=snip_bonds)
+
+
+mol = Chem.MolFromSmiles("[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O")
+highlight_cleave_sights2(mol)
