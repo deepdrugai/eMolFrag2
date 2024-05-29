9146872218eb3db1f61577f67cc4563bafb62edb
bessx <1159066+bessx@users.noreply.github.com>
2024-05-28 17:22:18 -0700

    Split asserts in tests

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -31,7 +31,8 @@ def test_mol_file_parent_name(mols, file_names):
     """Testing the molecule object"""
     for m in mols:
         log.info(f"Parent name : {m.getParent()}; File Name: {m.getFileName()}")
-        assert m.getParent() is None and m.getFileName() in file_names
+        assert m.getParent() is None
+        assert m.getFileName() in file_names
 
 
 def test_clear_prop_to_sdf(mols):
@@ -41,10 +42,11 @@ def test_clear_prop_to_sdf(mols):
         mol_to_sdf = m._toSDF()
         log.info(f"RDKit Object: {type(mol_to_sdf)}")
         mol_clear = m.clearProperties()
-        assert mol_clear is None and isinstance(mol_to_sdf, str)
+        assert mol_clear is None
+        assert isinstance(mol_to_sdf, str)
 
 
-@pytest.mark.parametrize("mol_path1, mol_path2, expected", [
+@pytest.mark.parametrize(("mol_path1", "mol_path2", "expected"), [
     ("uniqueMol(SMI)/DB00415.smi", "uniqueMol(SMI)/DB01208.smi", False),
     ("similarPairSMI/1/DB01421.smi", "similarPairSMI/1/DB01421.smi", True),
     ])  # fmt: skip
@@ -54,7 +56,8 @@ def test_eq_mols(mol_path1, mol_path2, expected):
     mol1 = to_mol(path1)
     mol2 = to_mol(path2)
     log.info(f"Molecule as String: {str(mol1) == str(mol2)}")
-    assert (mol1 == mol2) == expected and (str(mol1) == str(mol2)) == expected
+    assert (mol1 == mol2) == expected
+    assert (str(mol1) == str(mol2)) == expected
 
 
 def test_fragment_name(mols):
