079b055ed77a948d097c05a5e7aa2fb0814b81b2
bessx <1159066+bessx@users.noreply.github.com>
2024-06-13 22:29:20 -0700

    Improve logging format and path display

--- src/eMolFrag/utilities/logging.py
+++ src/eMolFrag/utilities/logging.py
@@ -5,16 +5,32 @@ global log
 try:  # if colorlog is installed, get colored log files
     import colorlog
 
-    colorlog.basicConfig(
-        format="%(log_color)s%(asctime)s [%(levelname)s: %(pathname)s:%(lineno)d::%(funcName)s] - %(message)s%(reset)s",
-        # format="%(log_color)s%(asctime)s [%(levelname)s: %(filename)s::%(funcName)s:%(lineno)d] - %(message)s%(reset)s",
+    class CustomFormatter(colorlog.ColoredFormatter):
+        def format(self, record):
+            # Modify record.pathname here before calling the parent class's format method
+            # This example gets the part of the path after "eMolFrag"
+            record.pathname = record.pathname.split("eMolFrag/")[-1]
+            return super().format(record)
+
+    # Then, when setting up your logger, use the custom formatter:
+    formatter = CustomFormatter(
+        fmt="%(log_color)s%(asctime)s [%(levelname)s: %(pathname)s:%(lineno)d::%(funcName)s] - %(message)s%(reset)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        # ,
-        # filename = "logfile.log",
-        # filemode = "w"
     )
+
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(formatter)
+
     log = colorlog.getLogger(__name__)
-except ImportError:  # pragma: no cover
+    log.addHandler(handler)
+except ImportError:  # pragma: no cover'
+    class CustomLogger(logging.getLoggerClass()):
+        def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None, sinfo=None):
+            record = super().makeRecord(name, level, fn, lno, msg, args, exc_info, func, extra, sinfo)
+            record.pathname = record.pathname.split("eMolFrag/")[-1]
+            return record
+
+    logging.setLoggerClass(CustomLogger)
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s: %(pathname)s:%(lineno)d::%(funcName)s] - %(message)s",
         # format="%(log_color)s%(asctime)s [%(levelname)s: %(filename)s::%(funcName)s:%(lineno)d] - %(message)s%(reset)s",
